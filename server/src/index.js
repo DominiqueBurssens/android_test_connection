0f7055aae47b09884a8e263d0807d1fb52b765e7
Lars Willemsens <lars.willemsens@kdg.be>
2018-05-09 16:47:44 +0200

    Added HTTPS server.

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,29 @@
 const express = require('express')
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+
 const app = express()
+
 const port = 3000
+const httpsPort = 4000
+
+const sslOptions = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+    passphrase: 'test'
+}
 
 app.get('/', (request, response) => {
     response.send('Connectie naar server werkt!')
 })
 
-app.listen(port, 'localhost', e =>
-    console.log(`Listening on localhost:${port}`)
+const httpServer = http.createServer(app)
+httpServer.listen(port, 'localhost', () =>
+    console.log(`Listening on localhost: ${port}`)
 )
 
-var sslOptions = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-  };
+const httpsServer = https.createServer(sslOptions, app)
+httpsServer.listen(httpsPort, 'localhost', () =>
+    console.log(`Listening securely on localhost: ${httpsPort}`)
+)
